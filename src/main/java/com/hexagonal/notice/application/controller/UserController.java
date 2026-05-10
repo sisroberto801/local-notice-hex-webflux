8a9e8b9571010cafc3ec3dd5af91fa71e4a7a132
rolguin <ext_roberto.olguin@cajalosandes.cl>
2026-05-10 00:04:19 -0400

    update requet in user controller

--- src/main/java/com/hexagonal/notice/application/controller/UserController.java
+++ src/main/java/com/hexagonal/notice/application/controller/UserController.java
@@ -2,22 +2,23 @@ package com.hexagonal.notice.application.controller;
 
 import com.hexagonal.notice.application.service.UserService;
 import com.hexagonal.notice.domain.model.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<User> createUser(@Validated @RequestBody User user) {
+    public Mono<User> createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
 
@@ -32,7 +33,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public Mono<User> updateUser(@PathVariable Long id, @Validated @RequestBody User user) {
+    public Mono<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         return userService.updateUser(id, user);
     }
 
